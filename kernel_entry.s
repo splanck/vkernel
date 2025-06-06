af1b39233ee6814beca2a1c9ce130b1de9e08043
Stephen Planck <splanck9271@gmail.com>
2025-06-06 17:39:43 -0400

    Add kernel entry assembly

---
+++ kernel_entry.s
@@ -0,0 +1,32 @@
+.intel_syntax noprefix
+.code64
+
+.global _start
+.extern kernel_main
+
+.section .text
+_start:
+    cli
+    # Load data segment selector (assume 0x10)
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    # Set up stack pointer
+    lea rsp, [stack_top]
+
+    call kernel_main
+
+hang:
+    hlt
+    jmp hang
+
+.section .bss
+    .align 16
+stack_bottom:
+    .skip 4096
+stack_top:
+
